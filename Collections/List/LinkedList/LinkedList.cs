8bc81f36f88acdcf81a62893f76c064d7813f7da
Mux <muxxum65536@gmail.com>
2020-11-29 11:45:54 +0900

    LinkedList init

--- Collections/List/LinkedList/LinkedList.cs
+++ Collections/List/LinkedList/LinkedList.cs
@@ -1,6 +1,74 @@
-﻿namespace MuxLib.MUtility.Collections.List.LinkedList
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace MuxLib.MUtility.Collections.List.LinkedList
 {
-    class LinkedList
+    public class LinkedList<T> : IList<T>
     {
+        private int _size;
+        private Node<T> _dummy_head;
+        private Node<T> _tail;
+
+        public int Count { get => _size; }
+
+        public bool IsReadOnly { set; get; } = false;
+
+        public T this[int index] { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+        public LinkedList()
+        {
+            _size = 0; _dummy_head = new Node<T>();
+            _tail = new Node<T>(null, _dummy_head);
+        }
+
+        public int IndexOf(T item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Insert(int index, T item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Add(T item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
--- Collections/List/LinkedList/Node.cs
+++ Collections/List/LinkedList/Node.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace MuxLib.MUtility.Collections.List.LinkedList
 {
-    internal class Node
+    internal class Node<T>
     {
+        public T Data { set; get; } = default;
+
+        public Node<T> Next { set; get; } = null;
+
+        public Node<T> Before { set; get; } = null;
+
+        public Node() { }
+
+        public Node(Node<T> next, Node<T> prev, T data) => (Next, Before, Data) 
+                                                        = (next, prev, data);
+
+        public Node(T data) { Data = data; }
+
+        public Node(Node<T> next, Node<T> prev) => (Next, Before) = (next, prev);
+
     }
 }
