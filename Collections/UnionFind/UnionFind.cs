fd75cb2bd74d3e3a64d50797165ed762272dfdbf
Mux <muxxum65536@gmail.com>
2020-11-30 15:03:18 +0900

    UnionFind added

---
+++ Collections/UnionFind/UnionFind.cs
@@ -0,0 +1,17 @@
+﻿namespace MuxLib.MUtility.Collections.UnionFind
+{
+    public class UnionFind : Metas.ABClass.ABCUnionFind
+    {
+        public override int Size => throw new System.NotImplementedException();
+
+        public override bool IsConnected(int item_p, int item_q)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void UnionElements(int item_p, int item_q)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
