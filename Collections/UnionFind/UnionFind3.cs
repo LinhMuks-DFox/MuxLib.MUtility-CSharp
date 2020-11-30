b8cf29078523ce12ed09b906efebbfef9493d513
Mux <muxxum65536@gmail.com>
2020-11-30 16:06:29 +0900

    UnionFind3 (UnionFind2.UionElements optimized with size)

---
+++ Collections/UnionFind/UnionFind3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MuxLib.MUtility.Collections.UnionFind
+{
+    public class UnionFind3 : Metas.ABClass.ABCUnionFind
+    {
+        public override int Size => throw new NotImplementedException();
+
+        public override bool IsConnected(int item_p, int item_q)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UnionElements(int item_p, int item_q)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
