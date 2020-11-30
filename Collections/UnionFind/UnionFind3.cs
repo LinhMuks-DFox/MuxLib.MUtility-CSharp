f4deb10fd79284fb25adafb7bd3487ca81d590ac
Mux <muxxum65536@gmail.com>
2020-11-30 16:07:03 +0900

    UnionFind3 (implemented)

--- Collections/UnionFind/UnionFind3.cs
+++ Collections/UnionFind/UnionFind3.cs
@@ -4,16 +4,39 @@ namespace MuxLib.MUtility.Collections.UnionFind
 {
     public class UnionFind3 : Metas.ABClass.ABCUnionFind
     {
-        public override int Size => throw new NotImplementedException();
+        private readonly int[] _parent;
 
-        public override bool IsConnected(int item_p, int item_q)
+        public UnionFind2(int size)
         {
-            throw new NotImplementedException();
+            _parent = new int[size];
+            for (int i = 0; i < size; i++)
+                _parent[i] = i;
         }
+
+        public override int Size { get => _parent.Length; }
+
+        // O(h)
+        private int Find(int p)
+        {
+            if (p < 0 && p >= _parent.Length)
+                throw new Errors.InvalidArgumentError($"Argument p:{p} is invalid.");
+            while (p != _parent[p])
+                p = _parent[p];
+
+            return p;
+        }
+
+        public override bool IsConnected(int item_p, int item_q) =>
+            Find(item_p) == Find(item_q);
+
 
         public override void UnionElements(int item_p, int item_q)
         {
-            throw new NotImplementedException();
+            int pRoot = Find(item_p), qRoot = Find(item_q);
+            if (pRoot == qRoot)
+                return;
+            _parent[pRoot] = qRoot;
         }
     }
+}
 }
