924838f371e12e599254ce1d32aed88639f482ed
Mux <muxxum65536@gmail.com>
2020-11-30 15:19:00 +0900

    Union Find. Find Done

--- Collections/UnionFind/UnionFind.cs
+++ Collections/UnionFind/UnionFind.cs
@@ -2,7 +2,27 @@
 {
     public class UnionFind : Metas.ABClass.ABCUnionFind
     {
-        public override int Size => throw new System.NotImplementedException();
+        private int[] _parent;
+
+        public UnionFind(int size)
+        {
+            _parent = new int[size];
+            for (int i = 0; i < size; i++)
+                _parent[i] = i;
+        }
+
+        public override int Size { get => _parent.Length; }
+
+        // O(h)
+        private int Find(int p)
+        {
+            if (p < 0 && p >= _parent.Length)
+                throw new Errors.InvalidArgumentError($"Argument p:{p} is invalid.");
+            while (p != _parent[p])
+                p = _parent[p];
+
+            return p;
+        }
 
         public override bool IsConnected(int item_p, int item_q)
         {
