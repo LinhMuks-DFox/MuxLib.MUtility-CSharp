344c17e139a26cb2d4f20d00ccab63913cf5a561
Mux <muxxum65536@gmail.com>
2020-12-02 18:42:58 +0900

    AVL Tree added

---
+++ Collections/Tree/AVLTree/AVLNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MuxLib.MUtility.Collections.Tree.AVLTree
+{
+    internal class AVLNode<K, V>
+        where K : IComparable
+    {
+        public K Key { get; set; }
+        public V Value { get; set; }
+
+        public AVLNode<K, V> Left { set; get; } = null;
+        public AVLNode<K, V> Right { set; get; } = null;
+
+        public AVLNode(K key, V value)
+        {
+            Key = key; Value = value;
+        }
+    }
+}
---
+++ Collections/Tree/AVLTree/AVLTree.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuxLib.MUtility.Collections.Tree.AVLTree
+{
+    public class AVLTree<K, V>
+        where K :IComparable
+    {
+    }
+}
