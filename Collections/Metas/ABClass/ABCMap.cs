02d4014e7fcf1b8f526fba46a3ba6946cdaea573
Mux <muxxum65536@gmail.com>
2020-12-05 20:54:16 +0900

    Map, Set Abstract Class Added

---
+++ Collections/Metas/ABClass/ABCMap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuxLib.MUtility.Collections.Metas.ABClass
+{
+    public abstract class ABCMap<K, V>
+    {
+        public abstract void Add(K key, V value);
+        public abstract V Remove(K key);
+        public abstract bool Contains(K key);
+        public abstract V Get(K key);
+        public abstract void Set(K key, V newValue);
+        public abstract int GetSize { get; }
+        public abstract bool IsEmpty { get; }
+    }
+}
---
+++ Collections/Metas/ABClass/ABCSet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuxLib.MUtility.Collections.Metas.ABClass
+{
+    public abstract class ABCSet<E>
+    {
+
+        public abstract void Add(E e);
+        public abstract bool Contains(E e);
+        public abstract void Remove(E e);
+        public abstract int Size { get; }
+        public abstract bool IsEmpty { get; }
+    }
+}
