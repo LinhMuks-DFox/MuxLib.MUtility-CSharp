2c6caa7b7b43b5855d3d15deb6b9e3ee320e6abe
Mux <muxum65536@gmail.com>
2021-01-17 21:51:49 +0900

    R_AVLTree implementing

--- Collections/Tree/AVLTree/R_AVLTree.cs
+++ Collections/Tree/AVLTree/R_AVLTree.cs
@@ -1,12 +1,78 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks
+﻿using System.Collections.Generic;
+using MuxLib.MUtility.Com.Attr;
 namespace MuxLib.MUtility.Collections.Tree.AVLTree
 {
-    [[Unimplemented]]
-    public class R_AVLTree
+    [Unimplemented]
+    public class R_AVLTree<K, V> : Metas.ABClass.ABCOrderSymbolTable<K, V>
+        where K : System.IComparable
     {
+        public override void Remove(K key)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool Contains(K key)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool Empty => Size == 0;
+        public override int Size { get; }
+
+        public override V this[K key]
+        {
+            get => throw new System.NotImplementedException();
+            set => throw new System.NotImplementedException();
+        }
+
+        public override K Min()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override K Max()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override K Floor(K key)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override K Ceiling(K key)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override int Rank(K key)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override K Select(int k)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void DeleteMin()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void DeleteMax()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override int NumberOf(K low, K max)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override IEnumerable<K> SortedKeys(K low, K max)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
